c7ee29587249f1e7eb8d2796f0f364019e48055f
71478355@qq.com <71478355@qq.com>
2024-11-01 14:17:54 +0800

    Site updated: 2024-11-01 14:17:53

--- custom/js/location.js
+++ custom/js/location.js
@@ -21,14 +21,57 @@
         }
     }
 
+    const getIPs = (callback) => {
+        var ip_dups = {};
+        var RTCPeerConnection = window.RTCPeerConnection
+            || window.mozRTCPeerConnection
+            || window.webkitRTCPeerConnection;
+        var useWebKit = !!window.webkitRTCPeerConnection;
+        var mediaConstraints = {
+            optional: [{ RtpDataChannels: true }]
+        };
+        // 这里就是需要的ICEServer了
+        var servers = {
+            iceServers: [
+                { urls: "stun:stun.services.mozilla.com" },
+                { urls: "stun:stun.l.google.com:19302" },
+            ]
+        };
+        var pc = new RTCPeerConnection(servers, mediaConstraints);
+        function handleCandidate(candidate) {
+            var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+            var hasIp = ip_regex.exec(candidate)
+            if (hasIp) {
+                var ip_addr = ip_regex.exec(candidate)[1];
+                if (ip_dups[ip_addr] === undefined)
+                    callback(ip_addr);
+                ip_dups[ip_addr] = true;
+            }
+        }
+        // 网络协商的过程
+        pc.onicecandidate = function (ice) {
+            if (ice.candidate) {
+                handleCandidate(ice.candidate.candidate);
+            }
+        };
+        pc.createDataChannel("");
+        //创建一个SDP(session description protocol)会话描述协议 是一个纯文本信息 包含了媒体和网络协商的信息
+        pc.createOffer(function (result) {
+            pc.setLocalDescription(result, function () { }, function () { });
+        }, function () { });
+        setTimeout(function () {
+            var lines = pc.localDescription.sdp.split('\n');
+            lines.forEach(function (line) {
+                if (line.indexOf('a=candidate:') === 0)
+                    handleCandidate(line);
+            });
+        }, 1000);
+    }
     t.load = function (n) {
-        return e.localStorage && e.localStorage.getItem('token') ? function (e, n) { //211.139.68.58
-            return r(e, `https://restapi.amap.com/v3/ip?key=${encodeURIComponent("37d0d1b0adb57a6c9ff5cd1744ef6023")}&ip=${encodeURIComponent("211.139.68.58")}`)
-        }(n) : function (n) {
-            return r(function (t) {
-                e.localStorage.setItem('token', t.token), n(t)
-            }, `https://restapi.amap.com/v3/ip?key=${encodeURIComponent("37d0d1b0adb57a6c9ff5cd1744ef6023")}&ip=${encodeURIComponent("211.139.68.58")}`)
-        }(n)
+        getIPs((ip) => {
+            console.log('ip', ip)
+        });
+        return r(e, `https://restapi.amap.com/v3/ip?key=${encodeURIComponent("37d0d1b0adb57a6c9ff5cd1744ef6023")}&ip=${encodeURIComponent("211.139.68.58")}`)
     },
         e.diliweizhi = t
 }(window);
